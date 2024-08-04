d2a0ac76588a92c705600d80130ee4082a24541c
Antti Mattila <me@rarelyneeded.com>
2024-08-04 18:29:25 +0300

    async RSS fetching

--- lib/feedme/reader.ex
+++ lib/feedme/reader.ex
@@ -1,5 +1,6 @@
 defmodule Feedme.Reader do
   alias Feedme.Reader.Item
+  alias Feedme.Reader.Feed
   alias Feedme.Repo
 
   # TODO
@@ -9,31 +10,49 @@ defmodule Feedme.Reader do
   # Calculate MD5 hash for title + body to see if they're updated
 
   def refresh_feeds() do
-    url = "https://mjtsai.com/blog/feed/"
-    refresh_feed(url)
+    feeds = Repo.all(Feed)
+
+    Enum.each(feeds, fn feed ->
+      Task.start(fn -> refresh_feed(feed.id, feed.url) end)
+    end)
   end
 
-  def refresh_feed(url) when is_binary(url) do
+  def refresh_feed(feed_id, url) when is_binary(url) and is_integer(feed_id) do
+    IO.puts("Refreshing feed: #{url}")
     raw_feed = Req.get!(url)
-    {:ok, feed} = FastRSS.parse_rss(raw_feed.body)
-    items = Map.get(feed, "items")
-    handle_items(items)
+
+    with {:ok, feed} <- FastRSS.parse_rss(raw_feed.body) do
+      items = Map.get(feed, "items")
+      handle_items(items)
+      IO.puts("Done refreshing feed: #{url}")
+    else
+      {:error, _} ->
+        IO.puts("Error parsing feed: #{url}")
+    end
   end
 
   def handle_items(items) do
     Enum.each(items, fn item ->
-      item |> parse_item() |> Repo.insert()
+      item |> parse_item |> persist_item
     end)
   end
 
-  def parse_item(item) do
-    published_at = Timex.parse!(Map.get(item, "pub_date"), "{RFC1123}")
+  def persist_item(nil), do: nil
 
-    Item.changeset(%Item{}, %{
-      title: Map.get(item, "title"),
-      body: Map.get(item, "content"),
-      published_at: published_at,
-      url: Map.get(item, "link")
-    })
+  def persist_item(item) do
+    Repo.insert(item)
+  end
+
+  def parse_item(item) do
+    with {:ok, published_at} <- Timex.parse(Map.get(item, "pub_date"), "{RFC1123}") do
+      Item.changeset(%Item{}, %{
+        title: Map.get(item, "title"),
+        body: Map.get(item, "content"),
+        published_at: published_at,
+        url: Map.get(item, "link")
+      })
+    else
+      {:error, _} -> IO.puts("Error parsing date: #{Map.get(item, "pub_date")}")
+    end
   end
 end
